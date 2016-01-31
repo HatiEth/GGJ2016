55467b5a1b72a60470c83b6d5743c94c846d98b1
Andreas Griesser <griesser@rhrk.uni-kl.de>
2016-01-31 21:20:29 +0100

    post jam commit

---
+++ Assets/ChangeSceneOnAnyKey.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ChangeSceneOnAnyKey : MonoBehaviour {
+
+    public string SceneName;
+    bool hasChanged = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.anyKeyDown && !hasChanged)
+        {
+            hasChanged = true;
+            SceneManager.LoadScene(SceneName);
+        }
+	}
+}
