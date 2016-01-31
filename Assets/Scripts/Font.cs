d4dbe8a26a37cc6d03829ffbe94356ed8a4cf705
Liech <nicolas_harttig@hotmail.de>
2016-01-31 13:17:25 +0100

    asd

---
+++ Assets/Scripts/Font.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Font : MonoBehaviour {
+
+    public List<GameObject> ObjectFont;
+
+    public string Get(string s)
+    {
+        //List<GameObject> Result = new List<GameObject>();
+        //for(int i = 0;i < s.Length;i++)
+        //{
+        //    s[i].
+        //}
+        return "Was";
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
